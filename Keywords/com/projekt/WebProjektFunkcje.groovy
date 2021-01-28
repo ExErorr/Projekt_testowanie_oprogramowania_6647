ca02a08267a53c1dc3a8ae8246dd6e0b5f94929a
ExErorr <58633014+ExErorr@users.noreply.github.com>
2021-01-28 17:01:56 +0100

    Add files via upload

---
+++ Keywords/com/projekt/WebProjektFunkcje.groovy
@@ -0,0 +1,78 @@
+package com.projekt
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testdata.TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords
+
+import internal.GlobalVariable
+
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.By
+
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory
+import com.kms.katalon.core.webui.driver.DriverFactory
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObjectProperty
+
+import com.kms.katalon.core.mobile.helper.MobileElementCommonHelper
+import com.kms.katalon.core.util.KeywordUtil
+
+import com.kms.katalon.core.webui.exception.WebElementNotFoundException
+
+
+class WebProjektFunkcje {
+	@Keyword
+	def generateRandomLogin() {
+		String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+		return randomString(chars,randomNumber(10,20));
+	}
+
+	@Keyword
+	def generateRandomLoginWithSpecialCharacters() {
+		String chars = '!@#$%^&*()'
+		return randomString(chars,randomNumber(10,20));
+	}
+
+	@Keyword
+	def generateRandomEmail() {
+		String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+		return randomString(chars,randomNumber(10,15)) + "@" + randomString(chars,randomNumber(10,15)) + ".com";
+	}
+
+
+
+
+
+	public static String randomString(String chars,int length) {
+
+		Random rand = new Random();
+		StringBuilder sb = new StringBuilder();
+		for (int i=0; i<length; i++) {
+			sb.append(chars.charAt(rand.nextInt(chars.length())));
+		}
+		return sb.toString();
+	}
+
+	public static int randomNumber(int min, int max) {
+		int random = (int )(Math.random() * max + min);
+		return random;
+	}
+}
